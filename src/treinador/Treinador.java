db2fd260e65353ef03582de166c041e9fcf706b0
harusspex <arturramos14041@gmail.com>
2026-09-20 16:05:52 -0300

    treinador detalhes

--- src/treinador/Treinador.java
+++ src/treinador/Treinador.java
@@ -8,23 +8,27 @@ public class Treinador {
     private nome String;
     private Pokesal pokesal;
     private Item[] itens;
-    private void melhoresCriticos(){
+    private void melhoresCriticos(Pokesal pokesal){
         pokesal.setCritico(pokesal.getCritico() + 10);
     };
-    private void melhorDesvio(){
+    private void melhorDesvio(Pokesal pokesal){
         pokesal.setDesvio(pokesal.getDesvio() + 5);
     };
-    private void melhorDefesa(){
+    private void melhorDefesa(Pokesal pokesal){
         pokesal.setDef(pokesal.getDef() + 5);
     };
     public void ataquePokesal(Pokesal inimigo){
         Random random = new Random();
         if( random.nextInt(1, 100) <= ( inimigo.getDesvio() + pokesal.getErro() ) ){
-            System.out.println("errou o golpe");
+            System.out.println(inimigo.getNome() +" errou o golpe!");
         }else if(random.nextInt() <= pokesal.getCritico()){
             inimigo.setHp(inimigo.getHp() - (pokesal.getAtk() * 2));
         }else {
             inimigo.setHp(inimigo.getHp() - pokesal.getAtk());
         }
     }
+    public Pokesal getPokesal(){
+        return this.pokesal;
+    }
+
 }
