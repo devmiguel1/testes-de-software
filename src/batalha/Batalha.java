f1bdf70273bd92295b69dadd3a7651f9c504e630
Miguel Antonio <miguel.antonio7001@gmail.com>
2026-09-20 16:33:58 -0300

    commit 6

--- src/batalha/Batalha.java
+++ src/batalha/Batalha.java
@@ -1,6 +1,10 @@
 package batalha;
 
 
+import treinador.Treinador;
+
+import java.util.Scanner;
+
 public class Batalha {
     public Terreno terreno;
     public Treinador treinador1;
@@ -10,36 +14,64 @@ public class Batalha {
     public void iniciarBatalha(Treinador t1, Treinador t2){
         boolean vida = true;
         while (vida){
-            if (t1.Pokesal.spd < t2.Pokesal.spd) {
+            if (t1.getPokesal().getSpd() < t2.getPokesal().getSpd()) {
                 vida = executarTurno(t2, t1);
             }
             else{
                 vida = executarTurno(t1, t2);
             }
         }
-
-
     }
 
     public boolean executarTurno(Treinador t1, Treinador t2){
+        Scanner sc = new Scanner(System.in);
+        String usarItem;
 
-        System.out.println("Ataque do " +t1.Pokesal.nome);
-        t1.ataque(t2);
 
-        if (t2.Pokesal.hp <= 0){
-            System.out.println(t2.Pokesal.nome +"morreu...");
-            System.out.println(t1.Pokesal.nome +"ganhou a batalha...");
-            return false;
+        System.out.println(treinador1.getNome() +"quer usar um dos itens? [S/N]");
+        usarItem = sc.nextLine();
+
+        if(usarItem.equalsIgnoreCase("S")){
+            escolhaItem(t1);
+        }else{
+            ataqueTurno(t1,t2);
+            if (t2.getPokesal().getHp() <= 0){
+                System.out.println(t2.getPokesal().getNome() +"morreu...");
+                System.out.println(t1.getPokesal().getNome() +"ganhou a batalha...");
+                return false;
+            }
         }
 
-        t2.ataque(t1);
-        System.out.println("Ataque do " +t2.Pokesal.nome);
+        System.out.println(treinador2.getNome() +"quer usar um dos itens? [S/N]");
+        usarItem = sc.nextLine();
 
-        if (t1.Pokesal.hp <= 0){
-            System.out.println(t1.Pokesal.nome +"morreu...");
-            System.out.println(t2.Pokesal.nome +"ganhou a batalha...");
-            return false;
+        if(usarItem.equalsIgnoreCase("S")){
+            escolhaItem(t2);
+        }else{
+            ataqueTurno(t2,t1);
+            if (t2.getPokesal().getHp() <= 0){
+                System.out.println(t2.getPokesal().getNome() +"morreu...");
+                System.out.println(t1.getPokesal().getNome() +"ganhou a batalha...");
+                return false;
+            }
         }
+
+    }
+
+    private void escolhaItem(Treinador t){
+        Scanner sc = new Scanner(System.in);
+        t.listarItens();
+        System.out.println("Ecolha o Item: ");
+        int escolha = sc.nextInt();
+        if (escolha == 1){
+            t.getItem(0).usar(t.getPokesal());
+        }
+    }
+
+    private void ataqueTurno(Treinador tacante, Treinador defensor) {
+        System.out.println("Ataque do " +tacante.getPokesal().getNome());
+        double bonus = terreno.bonus(treinador1,treinador2);
+        tacante.ataquePokesal(defensor.getPokesal());
     }
 
 
--- src/treinador/Treinador.java
+++ src/treinador/Treinador.java
@@ -1,30 +1,73 @@
 package treinador;
 
+import itens.Item;
 import pokesal.Pokesal;
 
 import java.util.Random;
 
 public class Treinador {
-    private nome String;
+    private String nome;
     private Pokesal pokesal;
-    private Item[] itens;
-    private void melhoresCriticos(){
+    private Item[] itens = new Item[2];
+
+    private void melhoresCriticos() {
         pokesal.setCritico(pokesal.getCritico() + 10);
-    };
-    private void melhorDesvio(){
+    }
+
+    ;
+
+    private void melhorDesvio() {
         pokesal.setDesvio(pokesal.getDesvio() + 5);
-    };
-    private void melhorDefesa(){
+    }
+
+    ;
+
+    private void melhorDefesa() {
         pokesal.setDef(pokesal.getDef() + 5);
-    };
-    public void ataquePokesal(Pokesal inimigo){
+    }
+
+    ;
+
+    public void ataquePokesal(Pokesal inimigo) {
         Random random = new Random();
-        if( random.nextInt(1, 100) <= ( inimigo.getDesvio() + pokesal.getErro() ) ){
+        if (random.nextInt(1, 100) <= (inimigo.getDesvio() + pokesal.getErro())) {
             System.out.println("errou o golpe");
-        }else if(random.nextInt() <= pokesal.getCritico()){
+        } else if (random.nextInt() <= pokesal.getCritico()) {
             inimigo.setHp(inimigo.getHp() - (pokesal.getAtk() * 2));
-        }else {
+        } else {
             inimigo.setHp(inimigo.getHp() - pokesal.getAtk());
         }
+    }
+
+    public void listarItens() {
+        System.out.println("[1] " +itens[0] +" [2] "+ itens[1]);
+    }
+
+    public Item[] getItens() {
+        return itens;
+    }
+
+    public Item getItem(int n) {
+        return itens[n];
+    }
+
+    public void setItens(Item[] itens) {
+        this.itens = itens;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Pokesal getPokesal() {
+        return pokesal;
+    }
+
+    public void setPokesal(Pokesal pokesal) {
+        this.pokesal = pokesal;
     }
 }
